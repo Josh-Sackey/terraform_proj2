38ad7715b546f9d4ac25b5873ef76d5ee17cf15a
yoophie97 <joshuasackey92@gmail.com>
2022-09-04 18:30:46 +0000

    add

--- proj2.tf
+++ proj2.tf
@@ -59,27 +59,27 @@ resource "aws_route_table_association" "a-rtb-subnet" {
 }
 
 resource "aws_security_group" "myapp-sg" {
-    name - "myapp-sg"
+    name = "myapp-sg"
     vpc_id = aws_vpc.myapp-vpc.id
 
     ingress {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_block = [var.myip]
+        cidr_blocks = [var.myip]
     }
     ingress {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
         from_port = 0
         to_port = 0
         protocol = "-1"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
         prefix_list_ids = []
     }
     tags = {
