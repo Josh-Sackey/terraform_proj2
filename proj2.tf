7b81f9130a189b766f362a5bf157912c31a42e54
yoophie97 <joshuasackey92@gmail.com>
2022-09-04 20:57:57 +0000

    add

--- proj2.tf
+++ proj2.tf
@@ -5,6 +5,9 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable myip {}
+variable ami {}
+variable instance_type {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -34,17 +37,6 @@ resource "aws_route_table"  "myapp-route-table" {
     }
 }
 
-# resource "aws_default_route_table" "main-rtb" {
-#     default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-
-#     route {
-#         cidr_block = "0.0.0.0/0"
-#         gateway_id = aws_internet_gateway.myapp-igw.id
-#     }
-#     tags = {
-#         Name = "${var.env_prefix}-main-rtb"
-#     }
-# }
 
 resource "aws_internet_gateway" "myapp-igw" {
     vpc_id = aws_vpc.myapp-vpc.id
@@ -85,4 +77,43 @@ resource "aws_security_group" "myapp-sg" {
     tags = {
         Name = "${var.env_prefix}-sg"
     }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = var.public_key_location
+}
+
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type= var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+    availability_zone = var.avail_zone
+    
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
 }
