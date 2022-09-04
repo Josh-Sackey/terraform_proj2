75b7613e239e4b372252adbde5a09c5108347b9f
yoophie97 <joshuasackey92@gmail.com>
2022-09-04 17:12:04 +0000

    update

--- main.tf
+++ main.tf
@@ -1,21 +1,22 @@
 provider "aws" {}
 
-variable cidr_block {
-    description = "cidr block for vpc and subnet"
-}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
-resource "aws_vpc" "development-vpc" {
-    cidr_block = var.cidr_block[0].cidr-block
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-        Name = var.cidr_block[0].name
+        Name = "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-subnet" {
-    vpc_id = aws_vpc.development-vpc.id
-    cidr_block = var.cidr_block[1].cidr-block
-    availability_zone = "us-east-1a"
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
     tags = {
-        Name = var.cidr_block[1].name
+        Name = "${var.env_prefix}-subnet-1"
     }
 }
