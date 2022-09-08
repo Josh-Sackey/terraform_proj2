65075148e429b7cd821fe68d6f9069f52ac21a91
yoophie97 <joshuasackey92@gmail.com>
2022-09-08 06:55:27 +0000

    add all

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable myip {}
+variable ami {}
+variable instance_type {}
+variable public_key_location {}
