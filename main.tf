6acf68a2909107d987dc825da170ff2798bfd8b7
yoophie97 <joshuasackey92@gmail.com>
2022-09-13 20:09:40 +0000

    modularize tf project

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-provider "aws" {}
+provider "aws" {
+    region = "us-east-1"
+}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -15,76 +17,15 @@ module "myapp-subnet" {
     vpc_id = aws_vpc.myapp-vpc.id
 }
 
-resource "aws_route_table_association" "a-rtb-subnet" {
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    route_table_id = aws_route_table.myapp-route-table.id
-}
-
-resource "aws_security_group" "myapp-sg" {
-    name = "myapp-sg"
+module "myapp-server" {
+    source = "./modules/webserver"
+    public_key_location = var.public_key_location
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
     vpc_id = aws_vpc.myapp-vpc.id
-
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.myip]
-    }
-    ingress {
-        from_port = 8080
-        to_port = 8080
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        prefix_list_ids = []
-    }
-    tags = {
-        Name = "${var.env_prefix}-sg"
-    }
-}
-
-data "aws_ami" "latest-amazon-linux-image" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
-    }
-    filter {
-        name = "virtualization-type"
-        values = ["hvm"]
-    }
-}
-
-resource "aws_key_pair" "ssh-key" {
-    key_name = "server-key"
-    public_key = var.public_key_location
-}
-
-
-resource "aws_instance" "myapp-server" {
-    ami = data.aws_ami.latest-amazon-linux-image.id
-    instance_type= var.instance_type
-
+    my_ip = var.my_ip
+    instance_type = var.instance_type
+    image_name = var.image_name
     subnet_id = module.myapp-subnet.subnet.id
-    vpc_security_group_ids = [aws_security_group.myapp-sg.id]
-    availability_zone = var.avail_zone
-    
-    associate_public_ip_address = true
-    key_name = aws_key_pair.ssh-key.key_name
-
-    user_data = file("entry-script.sh")
-    
-    tags = {
-        Name = "${var.env_prefix}-server"
-    }
-
 }
-
 
---
+++ modules/webserver/variables.tf
@@ -0,0 +1,8 @@
+variable public_key_location {}
+variable avail_zone {}
+variable env_prefix {}
+variable vpc_id {}
+variable my_ip {}
+variable instance_type {}
+variable image_name {}
+variable subnet_id {}
